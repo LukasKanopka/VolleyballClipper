aa7f98cd1468e8de2701d07ee2cce35c72311736
LukasKanopka <lukaskanopka@icloud.com>
2026-01-21 19:47:21 -0500

    Initial Commit

---
+++ VolleyballClipper/VolleyballClipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballClipperApp.swift
+//  VolleyballClipper
+//
+//  Created by Lukas Kanopka on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct VolleyballClipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VolleyballClipperTests/VolleyballClipperTests.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballClipperTests.swift
+//  VolleyballClipperTests
+//
+//  Created by Lukas Kanopka on 1/21/26.
+//
+
+import Testing
+@testable import VolleyballClipper
+
+struct VolleyballClipperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
